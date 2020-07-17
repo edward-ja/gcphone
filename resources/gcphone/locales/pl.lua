49bd9b98470934937192a7ceceb205fd77b8b3ab
edward-ja <68404828+edward-ja@users.noreply.github.com>
2020-07-17 15:09:37 +0200

    pl.lua
    added Polish locales

---
+++ resources/gcphone/locales/pl.lua
@@ -0,0 +1,9 @@
+Locales['pl'] = {
+    ["mission_row"] = "Posterunek Mission Row",
+    ["phone_booth"] = "Budka telefoniczna",
+    ["key_answer"] = "Wciśnij ~INPUT_PICKUP~ aby odebrać telefon",
+    ["new_message"] = "~o~Nowa wiadomość",
+    ["new_message_from"] = "~o~Nowa wiadomość od ~y~%s",
+    ["new_message_transmitter"] = "~o~Nowa wiadomość od ~g~%s",
+    ["use_fixed"] = "~g~%s telefon ~o~(%s) ~n~~INPUT_PICKUP~~w~ Użyj telefonu"
+}
